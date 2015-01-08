4e19e31d2ba640ffde956936c6c4f34d656666e0
Contron <connorhaigh@gmail.com>
2015-01-08 21:31:52 +0000

    Added a minimum size for the base form.

--- Sharpknife/Gui/Bases/BaseForm.Designer.cs
+++ Sharpknife/Gui/Bases/BaseForm.Designer.cs
@@ -35,6 +35,7 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.MinimumSize = new System.Drawing.Size(200, 200);
 			this.Name = "BaseForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Base Form";
