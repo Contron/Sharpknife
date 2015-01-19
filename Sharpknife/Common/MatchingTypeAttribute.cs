b375dae2cee0d842c2d77e1807a6e922c64073d1
Contron <connorhaigh@gmail.com>
2015-01-19 20:32:41 +0000

    Added an enum converter and matching attribute.

---
+++ Sharpknife/Common/MatchingTypeAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpknife.Common
+{
+	/// <summary>
+	/// Represents an attribute that can be used to match an appropriate type to anything.
+	/// This is most useful for enums that require an association with a specified type.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.All)]
+	public class MatchingTypeAttribute : Attribute
+	{
+		/// <summary>
+		/// Creates a new matching type attribute.
+		/// </summary>
+		/// <param name="type">the type</param>
+		public MatchingTypeAttribute(Type type)
+		{
+			this.Type = type;
+		}
+
+		/// <summary>
+		/// The actual type for the matching type.
+		/// </summary>
+		public Type Type
+		{
+			get;
+			private set;
+		}
+	}
+}
--- Sharpknife/Common/TypeCollectionEditor.cs
+++ Sharpknife/Common/TypeCollectionEditor.cs
@@ -42,7 +42,7 @@ namespace Sharpknife.Common
 		public Type Type
 		{
 			get;
-			set;
+			private set;
 		}
 	}
 }
