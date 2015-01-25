1365eb85e389e9d077f6ca4398d21deb9dd8bad3
Contron <connorhaigh@gmail.com>
2015-01-25 15:28:22 +0000

    Added missing documentation.

--- Sharpknife/Gui/Bases/BaseSettingsForm.cs
+++ Sharpknife/Gui/Bases/BaseSettingsForm.cs
@@ -8,11 +8,17 @@ using System.Windows.Forms;
 
 namespace Sharpknife.Gui.Bases
 {
+	/// <summary>
+	/// Represents a base settings form to allow the user to configure various settings for the application.
+	/// </summary>
 	public partial class BaseSettingsForm : BaseEntryForm
 	{
-		public BaseSettingsForm()
+		/// <summary>
+		/// Creates a new base settings form.
+		/// </summary>
+		public BaseSettingsForm() : base()
 		{
-			InitializeComponent();
+			this.InitializeComponent();
 		}
 	}
 }
