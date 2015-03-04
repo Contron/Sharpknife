923c40caddb7161b215c9740db9bea25fcfb9f44
Contron <connorhaigh@gmail.com>
2015-03-04 20:54:10 +0000

    Added some utility classes.

---
+++ Sharpknife/Core/Command.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sharpknife.Core
+{
+	/// <summary>
+	/// Represents an implementation of <see cref="ICommand" /> which can wrap a <see cref="Action" /> to act as a command.
+	/// </summary>
+	public class Command : ICommand
+	{
+		/// <summary>
+		/// Creates a new command.
+		/// </summary>
+		/// <param name="command">the command</param>
+		/// <param name="predicate">the predicate</param>
+		public Command(Action<object> command, Func<object, bool> predicate)
+		{
+			this.command = command;
+			this.predicate = predicate;
+		}
+
+		/// <summary>
+		/// Executes the command with the specified parameter.
+		/// </summary>
+		/// <param name="parameter">the parameter</param>
+		public void Execute(object parameter)
+		{
+			if (this.command == null)
+			{
+				return;
+			}
+
+			this.command(parameter);
+		}
+
+		/// <summary>
+		/// Returns if the command can execute with the specified parameter.
+		/// </summary>
+		/// <param name="parameter">the parameter</param>
+		/// <returns>if the command can execute</returns>
+		public bool CanExecute(object parameter)
+		{
+			if (this.predicate == null)
+			{
+				return false;
+			}
+
+			return this.predicate(parameter);
+		}
+
+		#region Event Triggers
+
+		private void OnCanExecuteChanged()
+		{
+			if (this.CanExecuteChanged != null)
+			{
+				this.CanExecuteChanged(this, EventArgs.Empty);
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Occurs when the can execute status changes.
+		/// </summary>
+		public event EventHandler CanExecuteChanged;
+
+		private Action<object> command;
+		private Func<object, bool> predicate;
+	}
+}
---
+++ Sharpknife/Core/Observable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpknife.Core
+{
+	/// <summary>
+	/// Represents an implementation of <see cref="INotifyPropertyChanged" /> which can be subclassed to provide notification support.
+	/// </summary>
+	public class Observable : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Notifies that the specified property has changed.
+		/// </summary>
+		/// <param name="property">the property</param>
+		protected void NotifyPropertyChanged([CallerMemberName] string property = null)
+		{
+			if (property == null)
+			{
+				throw new ArgumentNullException("property");
+			}
+
+			//trigger
+			this.OnPropertyChanged(property);
+		}
+
+		#region Event Triggers
+
+		private void OnPropertyChanged(string property)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(property));
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Occurs when a property changed.
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
