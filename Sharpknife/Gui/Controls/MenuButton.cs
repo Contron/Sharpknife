03a30feff6a261c4ca69aae6cdbed19ea8973906
Contron <connorhaigh@gmail.com>
2015-04-03 20:39:44 +0100

    Added a control to display a context menu when clicked.

---
+++ Sharpknife/Gui/Controls/MenuButton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+
+namespace Sharpknife.Gui.Controls
+{
+	/// <summary>
+	/// Represents a button that can have a <see cref="ContextMenu" /> attached to it when clicked.
+	/// </summary>
+	[ContentProperty("DisplayMenu")]
+	public class MenuButton : Button
+	{
+		/// <summary>
+		/// Creates a new menu button.
+		/// </summary>
+		public MenuButton()
+		{
+			this.DisplayMenu = null;
+		}
+
+		/// <summary>
+		/// Triggers the click event.
+		/// </summary>
+		protected override void OnClick()
+		{
+			base.OnClick();
+
+			if (this.DisplayMenu == null)
+			{
+				return;
+			}
+
+			if (!this.DisplayMenu.IsOpen)
+			{
+				//open
+				this.DisplayMenu.PlacementTarget = this;
+				this.DisplayMenu.Placement = PlacementMode.Bottom;
+				this.DisplayMenu.IsOpen = true;
+			}
+		}
+
+		/// <summary>
+		/// Gets the display menu property.
+		/// </summary>
+		public static readonly DependencyProperty DisplayMenuProperty = DependencyProperty.Register("DisplayMenu", typeof(ContextMenu), typeof(MenuButton), new UIPropertyMetadata(null));
+
+		/// <summary>
+		/// Gets or sets the display menu for the menu button.
+		/// </summary>
+		public ContextMenu DisplayMenu
+		{
+			get
+			{
+				return (ContextMenu) this.GetValue(MenuButton.DisplayMenuProperty);
+			}
+			set
+			{
+				this.SetValue(MenuButton.DisplayMenuProperty, value);
+			}
+		}
+	}
+}
