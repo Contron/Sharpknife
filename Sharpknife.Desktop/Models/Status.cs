8932c350c8139cb8adad74fd01254f4cb638e131
Contron <connorhaigh@gmail.com>
2016-01-17 14:56:02 +0000

    Added back the status box.

---
+++ Sharpknife.Desktop/Models/Status.cs
@@ -0,0 +1,75 @@
+﻿using Sharpknife.Desktop.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpknife.Desktop.Models
+{
+	/// <summary>
+	/// Represents the status of a window with a message and a busy status.
+	/// </summary>
+	public class Status : Observable
+	{
+		/// <summary>
+		/// Creates a new status.
+		/// </summary>
+		public Status()
+		{
+			this.Message = null;
+			this.Busy = false;
+
+			this.Reset();
+		}
+
+		/// <summary>
+		/// Updates the status with the specified message and busy state.
+		/// </summary>
+		/// <param name="message">the message</param>
+		/// <param name="busy">the busy state</param>
+		public void Update(string message, bool busy = false)
+		{
+			this.Message = message;
+			this.Busy = busy;
+		}
+
+		/// <summary>
+		/// Resets the status.
+		/// </summary>
+		public void Reset()
+		{
+			this.Update("Ready");
+		}
+
+		/// <summary>
+		/// Gets or sets the message.
+		/// </summary>
+		public string Message
+		{
+			get
+			{
+				return (string) this.Get();
+			}
+			set
+			{
+				this.Set(value);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets if the status is busy.
+		/// </summary>
+		public bool Busy
+		{
+			get
+			{
+				return (bool) this.Get();
+			}
+			set
+			{
+				this.Set(value);
+			}
+		}
+	}
+}
---
+++ Sharpknife.Desktop/Views/Controls/StatusBox.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Sharpknife.Desktop.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sharpknife.Desktop.Views.Controls
+{
+	/// <summary>
+	/// Represents a status box with a message and progress bar, to be paired with a <see cref="Status" />.
+	/// </summary>
+	[ContentProperty("Children")]
+	public partial class StatusBox : UserControl
+	{
+		/// <summary>
+		/// Creates a new status box.
+		/// </summary>
+		public StatusBox()
+		{
+			this.Status = new Status();
+			this.Children = new ObservableCollection<UIElement>();
+
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Gets the status property.
+		/// </summary>
+		public static readonly DependencyProperty StatusProperty = DependencyProperty.Register("Status", typeof(Status), typeof(StatusBox));
+
+		/// <summary>
+		/// Gets the children property.
+		/// </summary>
+		public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register("Children", typeof(ObservableCollection<UIElement>), typeof(StatusBox));
+
+		/// <summary>
+		/// Gets or sets the status.
+		/// </summary>
+		public Status Status
+		{
+			get
+			{
+				return (Status) this.GetValue(StatusBox.StatusProperty);
+			}
+			set
+			{
+				this.SetValue(StatusBox.StatusProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the children.
+		/// </summary>
+		public ObservableCollection<UIElement> Children
+		{
+			get
+			{
+				return (ObservableCollection<UIElement>) this.GetValue(StatusBox.ChildrenProperty);
+			}
+			set
+			{
+				this.SetValue(StatusBox.ChildrenProperty, value);
+			}
+		}
+	}
+}
