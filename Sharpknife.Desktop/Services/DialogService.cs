83ef2bc850da7e695d89923bea0532214d0acecf
Contron <connorhaigh@gmail.com>
2016-01-20 23:28:09 +0000

    Moved it back into the dialog service.

--- Sharpknife.Desktop/Services/DialogService.cs
+++ Sharpknife.Desktop/Services/DialogService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace Sharpknife.Desktop.Services
 {
@@ -17,6 +18,51 @@ namespace Sharpknife.Desktop.Services
 		private DialogService()
 		{
 
+		}
+
+		/// <summary>
+		/// Shows an open dialog with the specified filter.
+		/// </summary>
+		/// <param name="filter">the filter</param>
+		/// <returns>the path</returns>
+		public string ShowOpenDialog(string filter = null)
+		{
+			var dialog = new OpenFileDialog()
+			{
+				Title = "Open",
+				Filter = filter,
+				CheckPathExists = true,
+				CheckFileExists = true
+			};
+
+			var result = dialog.ShowDialog(WindowService.Instance.GetActive());
+
+			if (result != null)
+			{
+				if (result.Value)
+				{
+					return dialog.FileName;
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Show a save dialog with the specified filter.
+		/// </summary>
+		/// <param name="filter">the filter</param>
+		public void ShowSaveDialog(string filter = null)
+		{
+			var dialog = new SaveFileDialog()
+			{
+				Title = "Save",
+				Filter = filter,
+				CreatePrompt = true,
+				OverwritePrompt = true
+			};
+
+			dialog.ShowDialog(WindowService.Instance.GetActive());
 		}
 
 		/// <summary>
