aaeb8f295953b989f02eef020ad22031bc3a8195
Contron <connorhaigh@gmail.com>
2016-01-29 10:28:09 +0000

    Added an arguments service for command-line args.

---
+++ Sharpknife.Desktop/Services/ArgumentsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpknife.Desktop.Services
+{
+	/// <summary>
+	/// Represents an arguments service to retrieve application command-line arguments.
+	/// </summary>
+	public class ArgumentsService
+	{
+		private ArgumentsService()
+		{
+
+		}
+
+		/// <summary>
+		/// Gets the arguments supplied to the application, minus the name.
+		/// </summary>
+		/// <returns></returns>
+		public List<string> Get()
+		{
+			var arguments = Environment.GetCommandLineArgs().ToList();
+
+			if (arguments.Count > 0)
+			{
+				arguments.RemoveAt(0);
+			}
+
+			return arguments;
+		}
+
+		/// <summary>
+		/// Gets the instance of the arguments service.
+		/// </summary>
+		public static ArgumentsService Instance
+		{
+			get
+			{
+				return ArgumentsService.instance;
+			}
+		}
+
+		private static readonly ArgumentsService instance = new ArgumentsService();
+	}
+}
