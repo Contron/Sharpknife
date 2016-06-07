74f401073c25e988da77763ffd32a595ed34718f
Contron <connorhaigh@gmail.com>
2016-06-07 17:03:52 +0100

    Added class to work with memory.

---
+++ Sharpknife.Desktop/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sharpknife.Desktop.Helpers
+{
+	public class PasswordBoxHelper
+	{
+		public bool GetAttached(DependencyObject instance)
+		{
+			return (bool) instance.GetValue(PasswordBoxHelper.AttachedProperty);
+		}
+
+		public static void SetAttached(DependencyObject instance, bool value)
+		{
+			instance.SetValue(PasswordBoxHelper.AttachedProperty, value);
+		}
+
+		public string GetPassword(DependencyObject instance)
+		{
+			return (string) instance.GetValue(PasswordBoxHelper.PasswordProperty);
+		}
+
+		public static void SetPassword(DependencyObject instance, string value)
+		{
+			instance.SetValue(PasswordBoxHelper.PasswordProperty, value);
+		}
+
+		private static void AttachedChanged(DependencyObject instance, DependencyPropertyChangedEventArgs args)
+		{
+			var passwordBox = instance as PasswordBox;
+
+			if (passwordBox != null)
+			{
+				var value = args.NewValue as bool?;
+
+				if (value != null)
+				{
+					if (value.Value)
+					{
+						passwordBox.PasswordChanged += PasswordBoxHelper.PasswordBoxPasswordChanged;
+					}
+					else
+					{
+						passwordBox.PasswordChanged -= PasswordBoxHelper.PasswordBoxPasswordChanged;
+					}
+				}
+			}
+		}
+
+		private static void PasswordChanged(DependencyObject instance, DependencyPropertyChangedEventArgs args)
+		{
+			var passwordBox = instance as PasswordBox;
+
+			if (passwordBox != null)
+			{
+				var value = args.NewValue as string;
+
+				if (value != null)
+				{
+					passwordBox.Password = value;
+				}
+			}
+		}
+
+		private static void PasswordBoxPasswordChanged(object sender, RoutedEventArgs args)
+		{
+			var passwordBox = sender as PasswordBox;
+
+			if (passwordBox != null)
+			{
+				PasswordBoxHelper.SetPassword(passwordBox, passwordBox.Password);
+			}
+		}
+
+		public static readonly DependencyProperty AttachedProperty = DependencyProperty.RegisterAttached("Attached", typeof(bool), typeof(PasswordBoxHelper), new FrameworkPropertyMetadata(false, PasswordBoxHelper.AttachedChanged));
+		public static readonly DependencyProperty PasswordProperty = DependencyProperty.RegisterAttached("Password", typeof(string), typeof(PasswordBoxHelper), new FrameworkPropertyMetadata(string.Empty, PasswordBoxHelper.PasswordChanged));
+	}
+}
---
+++ Sharpknife/Utilities/Memory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpknife.Utilities
+{
+	/// <summary>
+	/// A collection of methods to read and write process' memory.
+	/// </summary>
+	public static class Memory
+	{
+		/// <summary>
+		/// Opens the specified process for memory operations.
+		/// </summary>
+		/// <param name="process">the process</param>
+		/// <returns>the handle</returns>
+		public static IntPtr Open(Process process)
+		{
+			return Memory.OpenProcess(0x0010 | 0x0020 | 0x0008, false, process.Id);
+		}
+
+		/// <summary>
+		/// Reads the specified number of bytes from the specified process' address.
+		/// </summary>
+		/// <param name="handle">the process' handle</param>
+		/// <param name="address">the address</param>
+		/// <param name="length">the length to read</param>
+		/// <returns>the buffer</returns>
+		public static byte[] Read(IntPtr handle, IntPtr address, int length)
+		{
+			var buffer = new byte[length];
+			var read = 0;
+
+			Memory.ReadProcessMemory(handle.ToInt32(), address.ToInt32(), buffer, buffer.Length, ref read);
+
+			return buffer;
+		}
+
+		/// <summary>
+		/// Writes the specified buffer to the specified process' address.
+		/// </summary>
+		/// <param name="handle">the process' handle</param>
+		/// <param name="address">the address</param>
+		/// <param name="buffer">the buffer</param>
+		public static void Write(IntPtr handle, IntPtr address, byte[] buffer)
+		{
+			var written = 0;
+
+			Memory.WriteProcessMemory(handle.ToInt32(), address.ToInt32(), buffer, buffer.Length, ref written);
+		}
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+		[DllImport("kernel32.dll", SetLastError = true)]
+		public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+	}
+}
