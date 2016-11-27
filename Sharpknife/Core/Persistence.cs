97fdbe44a7d17c5bd6fcf730d33665201aefaf66
Contron <connorhaigh@gmail.com>
2016-11-27 12:30:24 +0000

    Small update to persistence.

--- Sharpknife/Core/Persistence.cs
+++ Sharpknife/Core/Persistence.cs
@@ -16,17 +16,11 @@ namespace Sharpknife.Core
 	public class Persistence<T> where T : class, new()
 	{
 		/// <summary>
-		/// Creates a new persistence container with the specified path and file name.
+		/// Creates a new persistence container with the specified name.
 		/// </summary>
 		/// <param name="path">the path</param>
-		/// <param name="name">the file name</param>
-		public Persistence(string path, string name)
+		public Persistence(string name)
 		{
-			if (path == null)
-			{
-				throw new ArgumentNullException(nameof(path));
-			}
-
 			if (name == null)
 			{
 				throw new ArgumentNullException(nameof(name));
@@ -34,9 +28,7 @@ namespace Sharpknife.Core
 
 			this.Instance = null;
 
-			this.path = path;
 			this.name = name;
-
 			this.settings = new XmlWriterSettings()
 			{
 				Indent = true,
@@ -46,18 +38,9 @@ namespace Sharpknife.Core
 		}
 
 		/// <summary>
-		/// Creates a new persistence container with the specified path.
-		/// </summary>
-		/// <param name="path">the path</param>
-		public Persistence(string path) : this(path, typeof(T).Name)
-		{
-
-		}
-
-		/// <summary>
 		/// Creates a new persistence container using the default path and name.
 		/// </summary>
-		public Persistence() : this("Configuration")
+		public Persistence() : this(typeof(T).Name)
 		{
 
 		}
@@ -67,7 +50,9 @@ namespace Sharpknife.Core
 		/// </summary>
 		public void Load()
 		{
-			if (!File.Exists(this.Path))
+			var path = this.GetPath();
+
+			if (!File.Exists(path))
 			{
 				this.Instance = new T();
 
@@ -76,7 +61,7 @@ namespace Sharpknife.Core
 
 			var serializer = new XmlSerializer(typeof(T));
 
-			using (var stream = File.Open(this.Path, FileMode.Open))
+			using (var stream = File.Open(path, FileMode.Open))
 			{
 				this.Instance = serializer.Deserialize(stream) as T;
 			}
@@ -87,18 +72,30 @@ namespace Sharpknife.Core
 		/// </summary>
 		public void Save()
 		{
-			if (!Directory.Exists(this.path))
+			var directory = this.GetDirectory();
+
+			if (!Directory.Exists(directory))
 			{
-				Directory.CreateDirectory(this.path);
+				Directory.CreateDirectory(directory);
 			}
 
 			var serializer = new XmlSerializer(typeof(T));
 
-			using (var stream = File.Open(this.Path, FileMode.Create))
+			using (var stream = File.Open(this.GetPath(), FileMode.Create))
 			using (var writer = XmlWriter.Create(stream, this.settings))
 			{
 				serializer.Serialize(writer, this.Instance);
 			}
+		}
+
+		private string GetDirectory()
+		{
+			return "Configuration";
+		}
+
+		private string GetPath()
+		{
+			return Path.Combine(this.GetDirectory(), $"{this.name}.xml");
 		}
 
 		/// <summary>
@@ -110,18 +107,7 @@ namespace Sharpknife.Core
 			set;
 		}
 
-		private string path;
 		private string name;
-
 		private XmlWriterSettings settings;
-
-		private string Path
-		{
-			get
-			{
-				return System.IO.Path.Combine(this.path, $"{this.name}.xml");
-			}
-		}
-
 	}
 }
